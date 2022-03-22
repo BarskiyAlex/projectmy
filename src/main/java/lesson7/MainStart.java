08126f4b65e473cc03801cd98ee10efadaeb9535
masterX14 <shilaev1111@gmail.com>
2022-03-22 17:15:03 +0500

    lesson8

--- src/main/java/lesson7/MainStart.java
+++ src/main/java/lesson7/MainStart.java
@@ -1,20 +1,32 @@
 package lesson7;
 
+import java.util.Scanner;
+
 public class MainStart {
-    public static void main(String[] args) throws InterruptedException {
-        Cats cat = new Cats("Томас");
-        Plate plate = new Plate(10);
+    public static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int action;
+        Cat[] cats = new Cat[3];
+        cats[0] = new Cat("Марс", 6, 20);
+        cats[1] = new Cat("Томас", 8, 20);
+        cats[2] = new Cat("Рыжик", 12, 20);
 
-        plate.printInfo();
+        PlateTwo plateTwo = new PlateTwo(50);
+        plateTwo.info();
+        for (int i = 0; i < cats.length; i++) {
+            if (cats[i].satiety <= 20 && cats[i].satiety < plateTwo.food){
+                cats[i].eat(plateTwo);
+                cats[i].satiety = 20;
+                System.out.println("Котик " + cats[i].name + " покушал!");
+            } else {
+                System.out.println("Котик " + cats[i].name + " не поел!!!");
+            }
 
-//        while (plate.getFoodCount() > 0){
-//            cat.eat(plate);
-//            plate.printInfo();
-//            Thread.sleep(3000);
-//        }
-
-//        cat.eat(plate);
-//        plate.printInfo();
-
+        }
+        plateTwo.info();
+        System.out.println("Сколько грамм добавить в миску? Введите значение: ");
+        action = sc.nextInt();
+        plateTwo.increaseFood(action);
+        plateTwo.info();
     }
 }
